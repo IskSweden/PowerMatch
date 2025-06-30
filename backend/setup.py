a57b94e0708af0c630ada3188a706f115f1234d9
Isak Skoog <skoog.isak@gmail.com>
2025-06-30 12:07:43 +0200

    corrections

--- backend/setup.py
+++ backend/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="powermatch",
-    version="0.1.0",
+    version="0.1.3",
     author="Isak Skoog",
     description="Real-time energy precision game backend",
     packages=find_packages(),
