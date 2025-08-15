a3aa019513c0d462f17894c1bf41b30cb8f5966b
Isak Skoog <skoog.isak@gmail.com>
2025-08-15 13:54:24 +0200

    want to kill the backend too of course

--- scripts/start_powermatch.sh
+++ scripts/start_powermatch.sh
@@ -3,13 +3,13 @@
 cd ~/Documents/PowerMatch
 source powermatch-venv/bin/activate
 powermatch &
+POWERMATCH_PID=$!
 
 sleep 10
 
 URL="localhost:8000"
 
 DISPLAY=:0 chromium-browser --kiosk --incognito --no-first-run --disable-infobars "$URL" &
-
 CHROMIUM_PID=$!
 
 echo "Chromium launched with PID: $CHROMIUM_PID. Press Ctrl+P to close."
@@ -17,6 +17,8 @@ echo "Chromium launched with PID: $CHROMIUM_PID. Press Ctrl+P to close."
 sleep 3
 xdotool search --onlyvisible --class chromium windowfocus key --clearmodifiers --window %1 Ctrl+p
 
+echo "Closing Chromium (PID: $CHROMIUM_PID) and powermatch (PID: $POWERMATCH_PID)..."
 kill $CHROMIUM_PID
+kill $POWERMATCH_PID
 
-echo "Chromium closed."
+echo "All processes closed."
